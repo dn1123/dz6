1b803f6301b73949b97e0575228421103431b3fa
dn1123 <112434391+dn1123@users.noreply.github.com>
2022-10-07 17:55:21 +0300

    Add files via upload

---
+++ zad41/Program.cs
@@ -0,0 +1,52 @@
+﻿int InNumber()
+{
+    while (true)
+    {
+        string text = Console.ReadLine();
+        if (int.TryParse(text, out int number))
+        {
+            return number;
+        }
+        else
+        {
+            Console.Write("Не удалось распознать число. \nВведите число ещё раз: ");
+        }
+    }
+}
+
+void FillArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите число № {i + 1}: ");
+        array[i] = InNumber();
+    }
+}
+void PrintArray(double[] array)
+{
+    Console.Write("Массив заполнен: [ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.Write("]");
+}
+
+Console.Write("Введите размер массива: ");
+
+int size = InNumber();
+double[] array = new double[size];
+
+FillArray(array);
+
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+
+PrintArray(array);
+Console.WriteLine($"\nКоличество положительных чисел: {count}");
